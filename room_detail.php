6e0a8831f65bf2ed881d9e8509928e5d4c2b84f7
huynh <72492130+phandachuynh@users.noreply.github.com>
2021-05-01 22:42:13 +0700

    Create room_detail.php

---
+++ room_detail.php
@@ -0,0 +1,22 @@
+<div class="dealDetail__gallery">
+    <div class="row">
+        <div class="col-sm-8 col-lg-9 col-xs-12">
+            <div class="galleryGrid">
+                <div class="galleryGrid__item"><button type="button"><img src="" alt=""></button></div>
+            </div>
+        </div>
+        <div class="col-sm-4 col-lg-3 col-xs-12">
+            <div class="galleryInfo">
+                <div class="galleryInfo__price"><span>Giá chỉ từ:</span><span class="size-24 yellow-1 semibold"></span></div>
+                <div class="galleryInfo__btn"><button class="btn btn_orange">Mua ngay</button></div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="dealDetail__cont">
+    <div class="dealDetail__group">
+        <div class="dealDetail__info page-section" id="section_information">
+            <h5>Thông tin chi tiết</h5>
+        <div>
+    </div>
+</div>
